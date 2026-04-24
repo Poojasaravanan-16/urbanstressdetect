5c7c04ed2e21bbbbc07729695b57fa24aa3e4162
Pooja <saravananpooja96@gmail.com>
2026-04-24 13:57:47 +0530

    fix

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.dirname(__file__))
 
 from backend.app import app
 
